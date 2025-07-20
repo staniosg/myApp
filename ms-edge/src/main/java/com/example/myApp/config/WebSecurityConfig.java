2ba31470769245cf40cc7cf1b9abc20e749075cb
gStaniogloudis <v-gstaniogloudis@eurobank.com>
2025-07-20 21:54:15 +0300

    add custom login and logout handlers to WebSecurityConfig

--- ms-edge/src/main/java/com/example/myApp/config/WebSecurityConfig.java
+++ ms-edge/src/main/java/com/example/myApp/config/WebSecurityConfig.java
@@ -1,23 +1,53 @@
 package com.example.myApp.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 public class WebSecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // Register how to handle unauthorized access
+        http.setSharedObject(AuthenticationEntryPoint.class,
+                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/",
                                 "/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                        .failureHandler((request, response, exception) -> {
+                            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                            response.setContentType("application/json");
+                            response.getWriter().write("{\"error\": \"Login failed: " + exception.getMessage() + "\"}");
+                        })
+                        .successForwardUrl("/edge/user")
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+                            response.getWriter().write("Logout accepted");
+                        })
+                        .invalidateHttpSession(true)
+                )
+                // avoid unnecessary redirect
+                .requestCache().requestCache(new NullRequestCache())
+                //.httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 }
